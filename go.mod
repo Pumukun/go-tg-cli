edf3dd82974c67b4d8440a46631c4c36af46b933
pumukun <demgrisha@gmail.com>
2023-10-09 20:50:55 +0300

    tested tdlib

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Pumukun/go-tg-cli
 
 go 1.21.1
+
+require github.com/Arman92/go-tdlib v1.0.0
--- main.go
+++ main.go
@@ -1,8 +1,47 @@
-package gotgcli
+package main
 
-import "fmt"
+import (
+    "fmt"
+    "time"
+    
+    "github.com/Arman92/go-tdlib"
+)
 
 func main() {
-    fmt.Println("xd")
-}
+	tdlib.SetLogVerbosityLevel(1)
+	tdlib.SetFilePath("./errors.txt")
+	
+	client := tdlib.NewClient(tdlib.Config{
+		APIID:               "27134036",
+		APIHash:             "48e6be1f8cd5fc0f831bf657307f41e8",
+		//Phone:               "89031062562",
+		DatabaseDirectory:   "./tdlib-db",
+		//FilesDirectory:      "./tdlib-files",
+		UseMessageDatabase:  true,
+		UseSecretChats:      false,
+		SystemLanguageCode:  "en",
+		DeviceModel:         "Desktop",
+		SystemVersion:       "Unknown",
+		ApplicationVersion:  "1.0",
+		EnableStorageOptimizer: true,
+		IgnoreFileNames:       false,
+	})
+	
+	fmt.Println("TDLib client created")
+	
+	currentState, err := client.Authorize()
+	if err != nil {
+		fmt.Printf("Error getting current state: %v", err)
+		return
+	}
 
+	fmt.Printf("Current state: %v\n", currentState.GetAuthorizationStateEnum())
+	
+	for currentState.GetAuthorizationStateEnum() != tdlib.AuthorizationStateReadyType {
+		time.Sleep(2 * time.Second)
+		currentState, _ = client.Authorize()
+		fmt.Printf("Current state: %v\n", currentState.GetAuthorizationStateEnum())
+	}
+	
+	fmt.Println("Authorization ready! The user is logged in")
+}
